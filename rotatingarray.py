71dcc8c347b11c6e215e1338c56edcbc6a7e6999
BrahminiA <43632503+BrahminiA@users.noreply.github.com>
2019-07-15 14:33:58 +0530

    Update rotatingarray.py

--- rotatingarray.py
+++ rotatingarray.py
@@ -5,7 +5,8 @@ def rightRotate(lists, num):
 	for item in range(0, len(lists) - num): 
 		output_list.append(str(lists[item])) 
 	print(" ".join(output_list))
-rotate_num = int(input())
+n,rotate_num = input().split()
+rotate_num=int(rotate_num)
 list_1 = list(map(int,input().split()))
 
 rightRotate(list_1, rotate_num)
